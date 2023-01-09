19159e575048c916e526819ebecb983c9129741c
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-01-09 16:45:22 +0530

    added mui theme and added redux persist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import React, { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { themeSettings } from "theme";
 
 const App = () => {
-  return <div className="app">App</div>;
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
